46d6334ddcb5abebfee46ba36ba4707479bb3d1d
s2011r2593 <seanrhee2024@u.northwestern.edu>
2022-09-16 11:28:45 -0400

    parse csv

--- src/components/main/control-bar/Loader.tsx
+++ src/components/main/control-bar/Loader.tsx
@@ -1,12 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import uploadIcon from '../../../icons/file_upload.svg';
 
 const Loader = () => {
+  const [fileData, setFileData] = useState<number[][]>();
+
+  useEffect(() => {
+    fileData && console.table(fileData);
+  }, [fileData]);
+  
+  const reader = new FileReader;
+  reader.onload = (e) => {
+    let fileString = (e.target!.result as string).replaceAll('\r', '');
+
+    let data: number[][] = fileString.split('\n').map((i) => {
+      return i.split(',').map((j) => {
+        return parseFloat(j);
+      });
+    })
+
+    setFileData(data);
+  }
+
+  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    reader.readAsText(e.target.files![0]);
+  }
+
   return (
     <label>
       <HiddenInput
         type='file'
+        accept='.csv'
+        onChange={handleUpload}
       />
       <LoaderFacade>
         <Icon src={uploadIcon} />
